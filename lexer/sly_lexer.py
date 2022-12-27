3c3e3450e06f6fc64f071862b65a843c4b17ff1d
JavierOramas <javiale2000@gmail.com>
2022-12-26 22:49:24 -0500

    lexer with sly created with basic syntax, needs to be reviewed module init files created

---
+++ lexer/sly_lexer.py
@@ -0,0 +1,48 @@
+from sly import Lexer
+
+class TzScriptLexer(Lexer):
+
+    tokens = {ID,NUMBER,IF,ELSE,WHILE,PLUS,MINUS,TIMES,DIVIDE,EQ,ASSIGN,LE,LT,GE,GT,NE, VAR,CONTRACT,STRING,LEN,ENTRY,INPUT}
+    
+    # Tokens
+    @_(r'\d+')
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
+    
+    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    
+    # Special Cases
+    ID['if'] = IF
+    ID['else'] = ELSE
+    ID['while'] = WHILE
+    ID['var'] = VAR
+    ID['contract'] = CONTRACT
+    ID['string'] = STRING
+    ID["entry"] = ENTRY
+    ID["input"] = INPUT
+    ID['len'] = LEN
+
+    ignore = ' \t'
+    
+    # Symbols
+    literals = { ':', ';' , "\"", "\'", '{', '}' , '(', ')', ',' , '='}
+    PLUS    = r'\+'
+    MINUS   = r'-'
+    TIMES   = r'\*'
+    DIVIDE  = r'/'
+    EQ      = r'=='
+    ASSIGN  = r'='
+    LE      = r'<='
+    LT      = r'<'
+    GE      = r'>='
+    GT      = r'>'
+    NE      = r'!='
+    
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    def error(self, t):
+        print('Line %d: Bad character %r' % (self.lineno, t.value[0]))
+        self.index += 1
