2185a55acd1931f49ab1aa52e09d0d4deb11ef38
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2022-12-29 16:49:58 -0500

    Create class for handling tokens

---
+++ lex_token.py
@@ -0,0 +1,40 @@
+
+from grammar import Terminal
+
+
+class Token:
+    """
+    Basic token class.
+
+    Parameters
+    ----------
+    lex : str
+        Token's lexeme.
+    token_type : Enum
+        Token's type.
+    """
+
+    def __init__(self, lex: str, token_type: Terminal):
+        self.lex = lex
+        self.token_type = token_type
+
+    def __str__(self):
+        return f'{self.token_type}: {self.lex}'
+
+    def __repr__(self):
+        return str(self)
+
+    @property
+    def is_valid(self):
+        return True
+
+class UnknownToken(Token):
+    def __init__(self, lex):
+        Token.__init__(self, lex, None)
+
+    def transform_to(self, token_type):
+        return Token(self.lex, token_type)
+
+    @property
+    def is_valid(self):
+        return False
