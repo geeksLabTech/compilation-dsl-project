614deecd93ad92775cd441490801ec8f276efce5
Lia <liazerqueraf@gmail.com>
2023-01-03 00:12:10 -0500

    parser

--- parser/ll_parser.py
+++ parser/ll_parser.py
@@ -2,8 +2,8 @@
 
 from typing import Callable, Iterable, Iterator
 from grammar import AttributeProduction, Grammar, Sentence, Symbol, NonTerminal, Terminal, Production, EOF
-from lex_token import Token
-from utils import ContainerSet
+from lexer.lex_token import Token
+from parser.utils import ContainerSet, compute_firsts, compute_follows, compute_local_first
 
 
 class LLParser:
@@ -11,10 +11,9 @@ class LLParser:
         self.grammar = grammar
         self.parser: Callable[[list[Terminal]], list[Production]] = self.build_parser(grammar)
 
-
     def build_parser(self, grammar: Grammar):
-        firsts = self.compute_firsts(grammar)
-        follows = self.compute_follows(grammar, firsts)
+        firsts = compute_firsts(grammar)
+        follows = compute_follows(grammar, firsts)
         parsing_table = self.build_parsing_table(grammar, firsts, follows)
         def parser(w: list[Terminal]) -> list[Production]:
             assert grammar.startSymbol is not None, 'Start symbol cannot be None'
@@ -75,131 +74,6 @@ class LLParser:
                     synteticed[i]=self.__build_ast(next_production,left_parse,tokens,inherited[i])
                     P=attributes[0]
                     return P(inherited,synteticed) if P is not None else None
-
-    # Computes First(alpha), given First(Vt) and First(Vn) 
-    # alpha in (Vt U Vn)*
-    def compute_local_first(self, firsts, alpha: Sentence):
-        first_alpha = ContainerSet()
-        
-        try:
-            alpha_is_epsilon = alpha.IsEpsilon
-        except:
-            alpha_is_epsilon = False
-        
-        if alpha_is_epsilon:
-            first_alpha.set_epsilon()
-
-        else:
-            for i, x in enumerate(alpha):
-                if first_alpha.contains_epsilon:
-                    first_alpha.set_epsilon(False)
-                if x.IsTerminal:
-                    first_alpha.update(ContainerSet(x))
-                    break
-                else:
-                    first_alpha.hard_update(firsts[alpha[i]])
-                    if not first_alpha.contains_epsilon:
-                        break
-        
-        # alpha = X1 ... XN
-        # First(Xi) subconjunto First(alpha)
-        # epsilon pertenece a First(X1)...First(Xi) ? First(Xi+1) subconjunto de First(X) y First(alpha)
-        # epsilon pertenece a First(X1)...First(XN) ? epsilon pertence a First(X) y al First(alpha)
-        
-        return first_alpha
-
-    # Computes First(Vt) U First(Vn) U First(alpha)
-    # P: X -> alpha
-    def compute_firsts(self, G: Grammar):
-        firsts: dict[Symbol | Sentence, ContainerSet] = {}
-        change = True
-        
-        # init First(Vt)
-        for terminal in G.terminals:
-            firsts[terminal] = ContainerSet(terminal)
-            
-        # init First(Vn)
-        for nonterminal in G.nonTerminals:
-            firsts[nonterminal] = ContainerSet()
-        
-        while change:
-            change = False
-            
-            # P: X -> alpha
-            for production in G.Productions:
-                X = production.Left
-                alpha = production.Right
-                
-                # get current First(X)
-                first_X = firsts[X]
-                    
-                # init First(alpha)
-                try:
-                    first_alpha = firsts[alpha]
-                except KeyError:
-                    first_alpha = firsts[alpha] = ContainerSet()
-                
-                # CurrentFirst(alpha)???
-                local_first = self.compute_local_first(firsts, alpha)
-                
-                # update First(X) and First(alpha) from CurrentFirst(alpha)
-                change |= first_alpha.hard_update(local_first)
-                change |= first_X.hard_update(local_first)
-                        
-        # First(Vt) + First(Vt) + First(RightSides)
-        return firsts
-
-    def compute_follows(self, G: Grammar, firsts: dict[Symbol | Sentence, ContainerSet]):
-        follows: dict[Symbol|Sentence, ContainerSet] = { }
-        change = True
-        
-        # init Follow(Vn)
-        for nonterminal in G.nonTerminals:
-            follows[nonterminal] = ContainerSet()
-        
-        assert G.startSymbol is not None, 'Grammar has no start symbol'
-
-        follows[G.startSymbol] = ContainerSet(G.EOF)
-        
-        while change:
-            change = False
-            
-            # P: X -> alpha
-            for production in G.Productions:
-                X = production.Left
-                alpha = production.Right
-                
-                follow_X = follows[X]
-                
-                # X -> zeta Y beta
-                # First(beta) - { epsilon } subset of Follow(Y)
-                # beta ->* epsilon or X -> zeta Y ? Follow(X) subset of Follow(Y)
-                if alpha.IsEpsilon:
-                    continue
-
-
-                for i, symbol in enumerate(alpha):
-                    if(symbol.IsTerminal): 
-                        continue
-                    if(symbol.IsNonTerminal):
-                        
-                        if( i + 1 < len(alpha)):
-                            
-                            next_symbol = alpha[i+1]
-                            
-                                
-                            local_first = firsts[next_symbol]
-                            
-                                
-                            change |= follows[symbol].update(local_first)
-                            if local_first.contains_epsilon:
-                                change |= follows[symbol].update(follow_X)
-                        
-                        else:
-                            follows[alpha[-1]] = follow_X
-
-        # Follow(Vn)
-        return follows
 
     def build_parsing_table(self, G: Grammar, firsts: dict[Symbol | Sentence, ContainerSet], follows: dict[Symbol | Sentence, ContainerSet]):
         # init parsing table
---
+++ parser/slr_parser.py
@@ -0,0 +1,164 @@
+from parser.utils import compute_firsts, compute_follows, compute_local_first
+from grammar import Grammar, Sentence, Symbol, Item
+from automata import State
+
+class ShiftReduceParser:
+    SHIFT = 'SHIFT'
+    REDUCE = 'REDUCE'
+    OK = 'OK'
+    
+    def __init__(self, G, verbose=False):
+        self.G = G
+        self.verbose = verbose
+        self.action = {}
+        self.goto = {}
+        self._build_parsing_table()
+    
+    def _build_parsing_table(self):
+        raise NotImplementedError()
+
+    def __call__(self, w, get_shift_reduce=False):
+        stack = [ 0 ]
+        cursor = 0
+        output = []
+        operations = []
+        while True:
+            state = stack[-1]
+            lookahead = w[cursor]
+            if self.verbose: print(stack, '<---||--->', w[cursor:])
+            
+            if(state,lookahead) not in self.action:
+                print((state,lookahead))
+                print(self.action)
+                print("Error. Aborting...")
+                return None
+            
+            print('state, lookahead', state, lookahead)
+            print(self.action)
+            if self.action[state, lookahead] == self.OK:
+                action = self.OK
+            else:
+                action, tag = self.action[state, lookahead]
+            print('action, tsg', action)
+            if action==self.SHIFT:
+                operations.append(self.SHIFT)
+                stack+=[lookahead,tag]
+                cursor+=1
+            elif action==self.REDUCE:    
+                operations.append(self.REDUCE)
+                output.append(tag)
+                print('tag', tag)
+                head,body=tag    
+                for symbol in reversed(body):
+                    print('stack', stack)
+                    stack.pop()    
+                    
+                    assert stack.pop()==symbol
+                    state=stack[-1]
+                    print(self.goto,'goto')
+                    print('output', output)
+                goto=self.goto[state,head]
+                stack+=[head,goto]
+            elif action==self.OK:
+                stack.pop()
+                assert stack.pop()==self.G.startSymbol
+                assert len(stack)==1    
+                return output if not get_shift_reduce else(output,operations)
+            else:    
+                raise Exception('Invalid action!!!')
+
+class SLR1Parser(ShiftReduceParser):
+    def _build_parsing_table(self):
+        G = self.G.AugmentedGrammar(True)
+        firsts = compute_firsts(G)
+        follows = compute_follows(G, firsts)
+        automaton = self._build_LR0_automaton(G).to_deterministic()
+        for i, node in enumerate(automaton):
+            if self.verbose: print(i, '\t', '\n\t '.join(str(x) for x in node.state), '\n')
+            node.idx = i
+        for i,production in enumerate(G.Productions,1):
+            production.j = i
+        for node in automaton:
+            idx = node.idx
+            for state in node.state:
+                item = state.state
+            
+                if len(item.production.Right)==item.pos:
+                        # print('elif')
+                    if item.production.Left == G.startSymbol:
+                        self.action[node.idx,G.EOF] = self.OK
+                    else:
+                        for terminal_follow in follows[item.production.Left]:
+                            tag = None
+                            for production in G.Productions:
+                                if production == item.production:
+                                    tag = production
+                            self.action[node.idx,terminal_follow] = self.REDUCE,tag
+                            
+                elif item.production.Right[item.pos].IsTerminal:
+                    next_node = node.get(item.NextSymbol.Name)
+                    self.action[node.idx,item.NextSymbol] = self.SHIFT,next_node.idx 
+                    
+                else:
+                    next_node = node.get(item.NextSymbol.Name)
+                    self.goto[node.idx,item.NextSymbol] = next_node.idx 
+               
+    def _build_LR0_automaton(self, G):
+        assert len(G.startSymbol.productions) == 1, 'Grammar must be augmented'
+
+        start_production = G.startSymbol.productions[0]
+        start_item = Item(start_production, 0)
+
+        automaton = State(start_item, True)
+
+        pending = [ start_item ]
+        visited = { start_item: automaton }
+
+        while pending:
+            
+            current_item = pending.pop()
+            if current_item.IsReduceItem:
+                continue
+            
+            next_symbol = current_item.NextSymbol
+            transitions = {}
+            epsilon_transitions = set()
+            
+            if current_item.NextSymbol.IsNonTerminal:
+                for prod in next_symbol.productions:
+                    item = Item(prod, 0)
+                    if item not in visited:
+                        visited[item] = State(item, True)
+                        pending.append(item)
+                    # '' denota epsilon-transicion
+                    epsilon_transitions.add(visited[item])
+                
+                next_item = current_item.NextItem()
+                state = State(next_item, True)
+                transitions[str(next_symbol)] = state
+                if next_item not in visited:
+                    visited[next_item] = state
+                    pending.append(next_item)
+            
+            else:
+                item = current_item.NextItem()
+                state = State(item, True)
+                if item not in visited:
+                    visited[item] = state
+                    pending.append(item)
+                transitions[str(next_symbol)] = state
+            
+            for x in transitions:
+                visited[current_item].add_transition(x, transitions[x])
+            
+            for x in epsilon_transitions:
+                visited[current_item].epsilon_transitions.add(x)
+        
+        return automaton
+
+    @staticmethod
+    def _register(table, key, value):
+        assert key not in table or table[key] == value, 'Shift-Reduce or Reduce-Reduce conflict!!!'
+        table[key] = value
+            
+
