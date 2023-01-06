1a52ce44afeab4d876120e34337349b459b57e6f
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2023-01-06 17:18:54 -0500

    Impl automated test for sly lexer in hello world

--- tests/test_sly_lexer.py
+++ tests/test_sly_lexer.py
@@ -1,6 +1,6 @@
 from lexer.sly_lexer import TzScriptLexer
 from lexer.lex_token import Token
-from parser.tzscript_grammar import TZSCRIPT_GRAMMAR, contract, idx, opar, cpar, colon, typex, equal, num, semi, entry, ocur, let, ccur
+from parser.tzscript_grammar import TZSCRIPT_GRAMMAR,idx, num, typex, contract, ifx, elsex,equal, plus, minus, star, div,semi, colon, comma, dot, opar, cpar, ocur, ccur,let, func,entry
 import pytest
 
 def test_sly_lexer_hello_world():
@@ -16,15 +16,22 @@ def test_sly_lexer_hello_world():
     }
     '''
     lexer = TzScriptLexer()
-    tokens = lexer.tokenize(hello_world)
-    
+    tokens = list(lexer.tokenize(hello_world))
     expected_tokens = [Token('contract', contract),Token('store_value',idx),Token('(',opar),Token('value',idx),Token(':',colon),Token('int',typex),Token(')',cpar),Token('{',ocur),Token('let',let),Token('storage',idx),Token(':',colon),Token('int',typex),Token('=',equal),Token('0',num),Token(';',semi),Token('entry',entry),Token('replace',idx),Token('(',opar),Token('new_value',idx),Token(':',colon),Token('int',typex),Token(')',cpar),Token('{',ocur),Token('storage',idx),Token('=',equal),Token('new_value',idx),Token(';',semi),Token('}',ccur),Token('}',ccur),Token('EOF',TZSCRIPT_GRAMMAR.EOF)]
     expected_lexes = [t.lex for t in expected_tokens if t.token_type != TZSCRIPT_GRAMMAR.EOF]
     expected_terminals = [t.token_type for t in expected_tokens if t.token_type != TZSCRIPT_GRAMMAR.EOF]
     lexes = [t.value for t in tokens]
-    terminals = [t.type for t in tokens]
+    map_to_terminals_names = {'CONTRACT': contract.Name, 'ID': idx.Name, 'COLON': colon.Name, 'SEMICOLON': semi.Name, 'COMMA': comma.Name, 'INTEGER': num.Name, 'LPAREN': opar.Name, 'RPAREN': cpar.Name, 'LBRACE': ocur.Name, 'RBRACE': ccur.Name, 'LBRACKET': opar.Name, 'RBRACKET': cpar.Name, 'OR': plus.Name, 'AND': star.Name, 'OPERATOR': equal.Name, 'TERMINAL': typex.Name, 'NONTERMINAL': idx.Name, 'ENTRY': entry.Name, 'FUNC': func.Name, 'LET': let.Name, 'IF': ifx.Name, 'ELSE': elsex.Name, 'TYPE': typex.Name, 'STRING': typex.Name, 'NAT': typex.Name, 'INT': typex.Name, 'OPTIONAL': typex.Name, 'BOOL': typex.Name}
+    terminals_names = []
+    for token in tokens:
+        print('hola')
+        print(token.type)
+        terminals_names.append(map_to_terminals_names[token.type])
+
+    terminals = [TZSCRIPT_GRAMMAR[t] for t in terminals_names]
     assert lexes == expected_lexes
     assert terminals == expected_terminals
+    
     
 # def func():
 #     data = '''
