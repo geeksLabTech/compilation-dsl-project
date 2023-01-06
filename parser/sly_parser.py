57a01dc1e0b91ac8aebaa68f50da2e12305f5ba7
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2023-01-06 17:08:26 -0500

    Update sly_parser.py

--- parser/sly_parser.py
+++ parser/sly_parser.py
@@ -1,71 +1,160 @@
-from sly import Parser
-from lexer.sly_lexer import TzScriptLexer
+from sly import Lexer
+# from grammar import *
+from grammar import Grammar
 
-class TzScriptParser(Parser):
+class TzScriptLexer(Lexer):
+            
+    reserved_words = [ 'contract','entry','func','let','if','else','const','type','for','in','string','nat','int','map','optional','bool','None','true','false','return','calledBy']
+    # Set of token names. This is always required
     
-    tokens = TzScriptLexer.tokens
-    
-    precedence = (
-        ('left', "+", '-'),
-        ('left', "*", '/'),
-        ('right', "UMINUS")
-    )
-    
-    def __init__(self):
-        self.env = {}
+    tokens = {
+        COLON,
+        SEMICOLON,
+        COMMA,
+        INTEGER,
+        LPAREN,
+        RPAREN,
+        LBRACE,
+        RBRACE,
+        LBRACKET,
+        RBRACKET,
+        ID,
+        OR,
+        AND,
+        OPERATOR,
+        TERMINAL,
+        NONTERMINAL,
+        EPSILON,
+        SENTENCE,
+        SENTENCELIST,
+        ATTRIBUTEPRODUCTION,
+        PRODUCTION,
+        GRAMMAR,
+        CONTRACT,
+        ENTRY,
+        FUNC,
+        LET,
+        IF,
+        ELSE,
+        CONST,
+        TYPE,
+        FOR,
+        IN,
+        STRING,
+        NAT,
+        INT,
+        MAP,
+        OPTIONAL,
+        BOOL,
+        NONE,
+        TRUE,
+        FALSE,
+        RETURN,
+        CALLEDBY
         
-    @_("")
-    def statement(self, p):
-        pass
+    }
     
-    @_("var_assign")
-    def statement(slef, p):
-        return p.var_assign
+    tokens.add(t for t in R_W)
+
+    # String containing ignored characters (spaces and tabs)
+    ignore = ' \t'
+
+    # Regular expression rules for tokens
+    COLON = r'\:'
+    SEMICOLON = r'\;'
+    COMMA = r'\,'
+    INTEGER = r'\d+'
+    LPAREN = r'\('
+    RPAREN = r'\)'
+    LBRACE = r'\{'
+    RBRACE = r'\}'
+    LBRACKET = r'\['
+    RBRACKET = r'\]'
+    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    OPERATOR = r'[=<>+-/%*]+'
+    OR = r'\|'
+    AND = r'\&'
+    TERMINAL = r'Terminal'
+    NONTERMINAL = r'NonTerminal'
+    EPSILON = r'Epsilon'
+    SENTENCE = r'Sentence'
+    SENTENCELIST = r'SentenceList'
+    ATTRIBUTEPRODUCTION = r'AttributeProduction'
+    PRODUCTION = r'Production'
+    GRAMMAR = r'Grammar'
     
-    @_("NAME '=' expr")
-    def var_assign(self, p):
-        return ('var_assign', p.NAME, p.expr)
+    # reserved Words
+    ID["contract"] = CONTRACT
+    ID['entry'] = ENTRY
+    ID['func'] = FUNC
+    ID['let'] = LET
+    ID['if'] = IF
+    ID['else'] = ELSE
+    ID['const'] = CONST
+    ID['type'] = TYPE
+    ID['for'] = FOR
+    ID['in'] = IN
+    ID['string'] = TYPE
+    ID['nat'] = TYPE
+    ID['int'] = TYPE
+    ID['map'] = Type
+    ID['optional'] = TYPE
+    ID['bool'] = TYPE
+    ID['None'] = TYPE
+    ID['true'] = TRUE
+    ID['false'] = FALSE
+    ID['return'] = RETURN
+    ID['calledBy'] = CALLEDBY
+
+    # Define a rule so we can track line numbers
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')    
+
+    def t_OPERATOR(self, t):
+        r'[=<>+-/*]+'
+        return t
     
-    @_("NAME '=' STRING")
-    def var_assign(self, p):
-        return ('var_assign', p.NAME, p.STRING)
-    
-    @_('expr')
-    def statement(self, p):
-        return (p.expr)
-    
-    @_("expr '+' expr")
-    def expr(self, p):
-        return ('add', p.expr0, p.expr1)
-    
-    @_("expr '-' expr")
-    def expr(self, p):
-        return ('sub', p.expr0, p.expr1)
-    
-    @_("expr '*' expr")
-    def expr(self, p):
-        return ('mul', p.expr0, p.expr1)
-    
-    @_("expr '/' expr")
-    def expr(self, p):
-        return ('div', p.expr0, p.expr1)
-    
-    @_("'-' expr  expr")
-    def expr(self, p):
-        return ('add', p.expr0, p.expr1)
-    
-    @_("ENTRY ID LPAREN params RPAREN {")
-    def entry(self,p):
-        return ("ENTRY", p.NAME, p.params)
-    
-    # Define a rule to parse the list of parameters
-    @_("NAME")
-    @_("parameters COMMA NAME")
-    def parameters(self, p):
-        if len(p) == 1:
-            # If there is only one parameter, return a singleton list
-            return [p.NAME]
-        else:
-            # If there are multiple parameters, append the new parameter to the list
-            p.parameters.append(p.NAME)
-            return p.parameters
+    def t_COLON(self, t):
+        r'\:'
+        return t
+
+    def t_SEMICOLON(self, t):
+        r'\;'
+        return t
+
+    def t_COMMA(self, t):
+        r'\,'
+        return t
+
+    def t_INTEGER(self, t):
+        r'\d+'
+        t.value = int(t.value)
+        return t
+
+    def t_LPAREN(self, t):
+        r'('
+        return t
+
+    def t_RPAREN(self, t):
+        r')'
+        return t
+
+    def t_LBRACE(self, t):
+        r'\{'
+        return t
+
+    def t_RBRACE(self, t):
+        r'\}'
+        return t
+
+    def t_LBRACKET(self, t):
+        r'\['
+        return t
+
+    def t_RBRACKET(self, t):
+        r'\]'
+        return t
+
+    def t_error(self, t):
+        raise ValueError(f'Invalid token: {t.value[0]}')
