6ea5c5a347a3c02c5b4cd98291a5b184456d7bea
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2023-01-06 12:34:08 -0500

    Implement function to build ast for slr parser

--- parser/slr_parser.py
+++ parser/slr_parser.py
@@ -162,3 +162,33 @@ class SLR1Parser(ShiftReduceParser):
         table[key] = value
             
 
+def build_slr_ast(right_parse, operations, tokens, eof):
+    if not right_parse or not operations or not tokens:
+        return
+
+    right_parse = iter(right_parse)
+    tokens = iter(tokens)
+    stack = []
+    for operation in operations:
+        if operation == ShiftReduceParser.SHIFT:
+            token = next(tokens)
+            stack.append(token.lex)
+        elif operation == ShiftReduceParser.REDUCE:
+            production = next(right_parse)
+            head, body = production
+            attributes = production.attributes
+            assert all(rule is None for rule in attributes[1:]), 'There must be only synteticed attributes.'
+            rule = attributes[0]
+
+            if len(body):
+                synteticed = [None] + stack[-len(body):]
+                value = rule(None, synteticed)
+                stack[-len(body):] = [value]
+            else:
+                stack.append(rule(None, None))
+        else:
+            raise Exception('Invalid action!!!')
+
+    assert len(stack) == 1
+    assert isinstance(next(tokens).token_type, eof)
+    return stack[0]
