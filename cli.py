af1b211817c3415c42e4e772b1d119fab8369b9b
JavierOramas <javiale2000@gmail.com>
2023-01-09 14:02:11 -0500

    cli unified code

--- cli.py
+++ cli.py
@@ -14,149 +14,8 @@ import typer
 app = Typer()
 
 
-
-@app.command()
-def build(file: str = Argument("", help="tzscript file to be parsed"),
-          out_file: str = Argument(None, help='michelson file to be generated')):
-    """ generates the .tz michelson script from the tzscript file specified """
-    total = 7
-
-    fibonacci = '''
-    contract get_fib_n(n:int){
-        let last_fib_calculated: int = 0;
-
-        func fib(n: int) : int{
-            if (n <= 1) {
-                return n;
-            }
-            else {
-                let a: int = n - 1;
-                let b: int = n - 2;
-                return fib(a) + fib(b);
-            }
-        }
-        
-        entry get_fib(n: int){
-            let result: int = fib(n);
-            last_fib_calculated = result;
-        }
-    }
-    '''
-
-    with typer.progressbar(length=total) as progress:
-        with open(file, "r") as f:
-            script = f.read()
-        script = fibonacci
-        # print(script)
-        # Tokenize Script
-        print("\nTokenizing Script", end="")
-        lexer = TzScriptLexer()
-        lexer_tokens = list(lexer.tokenize(script))
-        tokens = process_lexer_tokens(lexer_tokens)
-
-        print("... OK")
-        progress.update(1)
-
-        terminals = [token.token_type for token in tokens]
-        print('\nterminals', terminals)
-        # Parse tokenized Script
-        print("\nParsing Script", end="")
-        parser = SLR1Parser(TZSCRIPT_GRAMMAR, verbose=False)
-        # print(tokens)
-        # tokens = [Token('contract', contract), Token('store_value', idx), Token('(', opar), Token('value', idx), Token(':', colon), Token('int', typex), Token(')', cpar), Token('{', ocur), Token('let', let), Token('storage', idx), Token(':', colon), Token('int', typex), Token('=', equal), Token('0', num), Token(';', semi), Token(
-        #     'entry', entry), Token('replace', idx), Token('(', opar), Token('new_value', idx), Token(':', colon), Token('int', typex), Token(')', cpar), Token('{', ocur), Token('storage', idx), Token('=', equal), Token('new_value', idx), Token(';', semi), Token('}', ccur), Token('}', ccur), Token('EOF', TZSCRIPT_GRAMMAR.EOF)]
-
-        # terminals = [token.token_type for token in tokens]
-        derivation = parser(terminals, True)
-        if derivation is None:
-            print(
-                "Please re-run the command something unexpected (and unrelated to the parsing) happened")
-            return
-        productions, operations = derivation
-        print("... OK")
-        progress.update(1)
-
-        print("\nBuilding AST", end="")
-        ast = build_slr_ast(productions, operations, tokens)
-        print("... OK")
-        progress.update(1)
-
-        print("\nPerforming Type Check", end="")
-        type_visitor = TypeCheckVisitor()
-        type_result = type_visitor.visit_program(ast)
-        if not type_result:
-            print("Something Went Wrong")
-            return
-
-        print("... OK")
-        progress.update(1)
-
-        print("\nPerforming Scope Check", end="")
-        scope_visitor = ScopeCheckVisitor()
-        scope_result = scope_visitor.visit_program(ast)
-        if not scope_result:
-            print("Something Went Wrong")
-            return
-
-        print("... OK")
-        progress.update(1)
-
-        print("\nPerforming Semantic Check", end="")
-        semantic_visitor = SemanticCheckerVisitor()
-        semantic_result = semantic_visitor.visit(ast)
-        if semantic_result:
-            print("\nSomething Went Wrong")
-            return
-        print("... OK")
-        progress.update(1)
-
-        print("\nGenerating Michelson Code", end="")
-        # TODO uncomment this when code generation is working OK
-        # michelson_generator = MichelsonGeneratorVisitor()
-        # michelson_generator.visit_program(ast)
-        # michelson_result = michelson_geneator.result
-        michelson_result = "NOT IMPLEMENTED YET"
-        if out_file is None:
-            out_file = file[:file.find(".tzs")]+".tz"
-        with open(out_file, "w") as f:
-            f.write(michelson_result)
-        print(f"\nGenerated {out_file}")
-        progress.update(1)
-
-
-@app.command()
-def represent(file: str = Argument("", help="tzscript file to be parsed"),
-              out_file: str = Argument(None, help='michelson file to be generated')):
-    """ generates the .tzs.repr the string representation of the code in the input """
-    total = 7
-    fibonacci = '''
-    contract get_fib_n(n:int){
-        let last_fib_calculated: int = 0;
-
-        func fib(n: int):int{
-            if (n <= 1) {
-                return n;
-            }
-            else {
-                let a: nat = n - 1;
-                let b: int = n - 2;
-                return fib(a) + fib(b);
-            }
-        }
-        
-        entry get_fib(n: int){
-            let result: int = fib(n);
-            last_fib_calculated = result;
-        }
-    }
-    '''
-
-    with typer.progressbar(length=total) as progress:
-        with open(file, "r") as f:
-            script = f.read()
-
-        script = fibonacci
-        # print(script)
+def process(script: str):
+    with typer.progressbar(length=7) as progress:
         # Tokenize Script
         print("\nTokenizing Script", end="")
         lexer = TzScriptLexer()
@@ -226,15 +85,100 @@ def represent(file: str = Argument("", help="tzscript file to be parsed"),
             print("... OK")
         progress.update(1)
 
-        print("\nGenerating String representation Code")
-        string_repr = FormatVisitor()
-        result = string_repr.visit(ast)
-        if out_file is None:
-            out_file = file[:file.find(".tzs")]+".tzs.rep"
-        with open(out_file, "w") as f:
-            f.write(result)
-        progress.update(1)
-        print(f"\nGenerated {out_file}")
+        return ast, progress
+
+
+@app.command()
+def build(file: str = Argument("", help="tzscript file to be parsed"),
+          out_file: str = Argument(None, help='michelson file to be generated')):
+    """ generates the .tz michelson script from the tzscript file specified """
+
+    fibonacci = '''
+    contract get_fib_n(n:int){
+        let last_fib_calculated: int = 0;
+
+        func fib(n: int) : int{
+            if (n <= 1) {
+                return n;
+            }
+            else {
+                let a: int = n - 1;
+                let b: int = n - 2;
+                return fib(a) + fib(b);
+            }
+        }
+        
+        entry get_fib(n: int){
+            let result: int = fib(n);
+            last_fib_calculated = result;
+        }
+    }
+    '''
+    with open(file, "r") as f:
+        script = f.read()
+
+    script = fibonacci
+
+    ast, progress = process(script)
+
+    print("\nGenerating Michelson Code", end="")
+    # TODO uncomment this when code generation is working OK
+    # michelson_generator = MichelsonGeneratorVisitor()
+    # michelson_generator.visit_program(ast)
+    # michelson_result = michelson_geneator.result
+    michelson_result = "NOT IMPLEMENTED YET"
+    if out_file is None:
+        out_file = file[:file.find(".tzs")]+".tz"
+    with open(out_file, "w") as f:
+        f.write(michelson_result)
+    print(f"\nGenerated {out_file}")
+    progress.update(1)
+
+
+@app.command()
+def represent(file: str = Argument("", help="tzscript file to be parsed"),
+              out_file: str = Argument(None, help='michelson file to be generated')):
+    """ generates the .tzs.repr the string representation of the code in the input """
+    fibonacci = '''
+    contract get_fib_n(n:int){
+        let last_fib_calculated: int = 0;
+
+        func fib(n: int):int{
+            if (n <= 1) {
+                return n;
+            }
+            else {
+                let a: nat = n - 1;
+                let b: int = n - 2;
+                return fib(a) + fib(b);
+            }
+        }
+        
+        entry get_fib(n: int){
+            let result: int = fib(n);
+            last_fib_calculated = result;
+        }
+    }
+    '''
+
+    # with typer.progressbar(length=total) as progress:
+    with open(file, "r") as f:
+        script = f.read()
+
+        script = fibonacci
+        # print(script)
+        # Tokenize Script
+    ast, progress = process(script)
+
+    print("\nGenerating String representation Code")
+    string_repr = FormatVisitor()
+    result = string_repr.visit(ast)
+    if out_file is None:
+        out_file = file[:file.find(".tzs")]+".tzs.rep"
+    with open(out_file, "w") as f:
+        f.write(result)
+    progress.update(1)
+    print(f"\nGenerated {out_file}")
 
 
 @app.command()
