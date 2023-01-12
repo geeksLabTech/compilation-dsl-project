0cf80a96ff9f84ac6a7a01e72595b61fd90c7432
JavierOramas <javiale2000@gmail.com>
2023-01-11 20:39:52 -0500

    -

--- visitors/michelson_generator_visitor.py
+++ visitors/michelson_generator_visitor.py
@@ -1,68 +1,24 @@
-from visitors.visitor import Visitor
-from parser.tzscript_ast import *
+# from parser.tzscript_ast import *
+from intermediate_ast.high_level_ir_ast import *
+import visitors.visitor_d as visitor
 
-class MichelsonGeneratorVisitor(Visitor):
-    def __init__(self):
-        self.result = []
-    
-    def visit_program(self, node: ProgramNode):
-        self.result.append("parameter (pair")
-        for param in node.params:
-            self.result.append(param.accept(self))
-        self.result.append(")")
-        self.result.append("storage (pair")
-        for statement in node.statements:
-            self.result.append(statement.accept(self))
-        self.result.append(")")
-        return "\n".join(self.result)
 
-    def visit_if_node(self, node: IfNode):
-        self.result.append("if")
-        self.result.append(node.expr.accept(self))
-        self.result.append("then")
-        for statement in node.statements:
-            self.result.append(statement.accept(self))
-        self.result.append("else")
-        return "\n".join(self.result)
+class MichelsonGenerator(object):
+    @visitor.on('node')
+    def visit(self, node):
+        pass
 
-    def visit_else_node(self, node: ElseNode):
-        for statement in node.statements:
-            self.result.append(statement.accept(self))
-        return "\n".join(self.result)
+    @visitor.on(ContractNode)
+    def visit(self, node: ContractNode):
+        code = ""
 
-    def visit_var_declaration_node(self, node: VarDeclarationNode):
-        self.result.append(f"{node.id} : {node.type}")
-        self.result.append(node.expr.accept(self))
-        return "\n".join(self.result)
+        for i, entry_node in enumerate(node.entrypoints.entrypoint_list):
+            code += f"parameter {entry_node.i} "
+            # handle parameters
+            for param in entry_node.params:
+                code += f"({param.id}:{param.type})"
+            code += "\n"
 
-    def visit_assign_node(self, node: AssignNode):
-        self.result.append(f"{node.id} <- {node.expr.accept(self)}")
-        return "\n".join(self.result)
-
-    def visit_func_declaration_node(self, node: FuncDeclarationNode):
-        self.result.append(f"{node.id} (pair")
-        for param in node.params:
-            self.result.append(param.accept(self))
-        self.result.append(") : {node.type}")
-        for statement in node.body:
-            self.result.append(statement.accept(self))
-        return "\n".join(self.result)
-
-    def visit_attr_declaration_node(self, node: AttrDeclarationNode):
-        self.result.append(f"{node.id} : {node.type}")
-        return "\n".join(self.result)
-
-    def visit_atomic_node(self, node: AtomicNode):
-        return node.lex
-
-    def visit_binary_node(self, node: BinaryNode):
-        return f"{node.left.accept(self)} {node.right.accept(self)}"
-
-    def visit_call_node(self, node: CallNode):
-        func_name = node.id
-        args_code = []
-        for arg in node.args:
-            arg_code = arg.accept(self)
-            args_code.append(arg_code)
-
-        return f"{func_name} {' '.join(args_code)}"
+        for
+        
+        code += "}"
