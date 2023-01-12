6a73cb3e434928d98d9c31153bbc168c4d0fcec1
JavierOramas <javiale2000@gmail.com>
2023-01-11 23:11:37 -0500

    base

--- visitors/michelson_generator_visitor.py
+++ visitors/michelson_generator_visitor.py
@@ -13,12 +13,31 @@ class MichelsonGenerator(object):
         code = ""
 
         for i, entry_node in enumerate(node.entrypoints.entrypoint_list):
-            code += f"parameter {entry_node.i} "
+            code += f"entrypoint {entry_node.i} "
             # handle parameters
-            for param in entry_node.params:
-                code += f"({param.id}:{param.type})"
-            code += "\n"
+            code += "( "
+            if len(entry_node.params) >= 1:
+                for param in entry_node.params:
+                    code += f"parameter {param.type}; "
+            else:
+                code += f"unit;"
 
-        for
-        
+            if node.storage[i].storage_list:
+                for param in entry_node.params:
+                    code += f"{param.type}"
+            elif len(entry_node.params) == 1:
+                param = entry_node.params[0]
+                code += f"storage {param.type}; "
+            else:
+                code += f"unit"
+            code += ")\n"
+
+            code += "code{"
+
+            # code is for contract and no idea where is the entrypoint code
+            for st in node.code.statements:
+                self.visit(st)
+
+            code += "}\n"
+
         code += "}"
