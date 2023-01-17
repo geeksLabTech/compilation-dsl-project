b905a0d0500d4bd041b538cf8b74d85191da8c78
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2023-01-17 18:21:47 -0500

    Add type info to Scope class

--- visitors/scope.py
+++ visitors/scope.py
@@ -1,20 +1,29 @@
 
+from parser.tzscript_types import *
 
 class VariableInfo:
-    def __init__(self, name):
+    def __init__(self, name: str, type: TzScriptType):
         self.name = name
+        self.type = type
 
 class FunctionInfo:
-    def __init__(self, name, params):
+    def __init__(self, name: str, params_types: list[TzScriptType], return_type: TzScriptType):
         self.name = name
-        self.params = params
+        self.params_types = params_types
+        self.return_type = return_type
+
+class EntryInfo:
+    def __init__(self, name: str, params_types: list[TzScriptType]) -> None:
+        self.name = name
+        self.params_types = params_types
 
 class Scope:
     def __init__(self, parent=None):
         self.local_vars: list[VariableInfo] = []
         self.local_funcs: list[FunctionInfo] = []
-        self.parent = parent
-        self.children = []
+        self.entries: list[EntryInfo] = []
+        self.parent: Self | None = parent
+        self.children: list[Self] = []
         self.var_index_at_parent = 0 if parent is None else len(parent.local_vars)
         self.func_index_at_parent = 0 if parent is None else len(parent.local_funcs)
         self.main_level = False
@@ -32,11 +41,26 @@ class Scope:
         self.children.append(child_scope)
         return child_scope
 
-    def define_variable(self, vname):
-        self.local_vars.append(VariableInfo(vname))
+    def create_child_scope_with_parent_info(self):
+        child_scope = Scope(self)
+        for f in self.local_funcs:
+            child_scope.local_funcs.append(f)
+        for v in self.local_vars:
+            child_scope.local_vars.append(v)
+        for e in self.entries:
+            child_scope.entries.append(e)
+        
+        self.children.append(child_scope)
+        return child_scope
+
+    def define_variable(self, vname: str, type: TzScriptType):
+        self.local_vars.append(VariableInfo(vname, type))
     
-    def define_function(self, fname, params):
-        self.local_funcs.append(FunctionInfo(fname, params))
+    def define_function(self, fname, params, return_type: TzScriptType):
+        self.local_funcs.append(FunctionInfo(fname, params, return_type))
+
+    def define_entry(self, fname: str, params):
+        self.entries.append(EntryInfo(fname, params))
 
     def is_var_defined(self, vname):
         for var in self.local_vars:
@@ -49,9 +73,9 @@ class Scope:
         return self.parent.is_var_defined(vname)
     
     
-    def is_func_defined(self, fname, n):
+    def is_func_defined(self, fname: str, n: int):
         for f in self.local_funcs:
-            if f.name == fname and f.params == n:
+            if f.name == fname and f.params_types == n:
                 return True
         
         if self.parent is None:
@@ -59,13 +83,23 @@ class Scope:
 
         return self.parent.is_func_defined(fname, n)
 
-    def is_local_var(self, vname):
+    def is_entry_defined(self, fname: str, n: int):
+        for e in self.entries:
+            if e.name == fname and e.params_types == n:
+                return True
+        
+        if self.parent is None:
+            return False
+
+        return self.parent.is_entry_defined(fname, n)
+
+    def is_local_var(self, vname: str):
         return self.get_local_variable_info(vname) is not None
     
-    def is_local_func(self, fname, n):
+    def is_local_func(self, fname: str, n: int):
         return self.get_local_function_info(fname, n) is not None
 
-    def get_local_variable_info(self, vname):
+    def get_local_variable_info(self, vname: str):
         var_info = None 
         for var in self.local_vars:
             if var.name == vname:
@@ -74,14 +108,12 @@ class Scope:
         
         return var_info
     
-    def get_local_function_info(self, fname, n):
+    def get_local_function_info(self, fname: str, n: int):
         func_info = None 
         for func in self.local_funcs:
-            if func.name == fname and func.params:
+            if func.name == fname and func.params_types:
                 func_info = func 
                 break
         
         return func_info
-    
-    
-scope = Scope()
+
