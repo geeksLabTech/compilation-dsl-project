539a2b71d8c2240472ab9177891275bfe274e996
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2023-01-18 00:54:17 -0500

    Impl utility functions to run lexer and parser

--- utils.py
+++ utils.py
@@ -1,9 +1,33 @@
 
+from lexer.sly_lexer import TzScriptLexer, process_lexer_tokens 
+from parser.tzscript_grammar import TZSCRIPT_GRAMMAR
+from parser.slr_parser import SLR1Parser, build_slr_ast
 
 # from typing import Self
 from grammar import EOF, Epsilon, Grammar, Production, Sentence, Symbol
 import hashlib
 import base58
+
+
+def run_tzscript_parser_pipeline(script: str):
+    lexer = TzScriptLexer()
+    lexer_tokens = list(lexer.tokenize(script))
+    tokens = process_lexer_tokens(lexer_tokens)
+
+    parser = SLR1Parser(TZSCRIPT_GRAMMAR, verbose=True)
+
+    terminals = [token.token_type for token in tokens]
+    derivation = parser(terminals, True)
+    assert derivation is not None 
+    productions, operations = derivation
+    return productions, operations, tokens
+
+
+def run_tzscript_ast_building_pipeline(script: str):
+    productions, operations, tokens = run_tzscript_parser_pipeline(script)
+    ast = build_slr_ast(productions, operations, tokens)
+    assert ast is not None
+    return ast
 
 
 def inspect(item, grammar_name='G', mapper=None):
