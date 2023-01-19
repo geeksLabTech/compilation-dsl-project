28cbcebfdaf40ed51afdf488d09194e93665c97a
JavierOramas <javiale2000@gmail.com>
2023-01-19 10:21:26 -0500

    not equal added

--- lexer/sly_lexer.py
+++ lexer/sly_lexer.py
@@ -2,7 +2,7 @@ from sly import Lexer
 # from grammar import *
 from grammar import Grammar
 
-from parser.tzscript_grammar import TZSCRIPT_GRAMMAR, idx, num, typex, contract, ifx, elsex, equal, plus, returnx, minus, star, div, semi, colon, comma, dot, opar, cpar, ocur, ccur, let, func, entry, equalequal, lessthan, greaterthan, lessthanequal, greaterthanequal, dquoutes, stringx, whilex
+from parser.tzscript_grammar import TZSCRIPT_GRAMMAR, idx, num, typex, contract, ifx, elsex, equal, plus, returnx, minus, star, div, semi, colon, comma, dot, opar, cpar, ocur, ccur, let, func, entry, iniquelaty, equalequal, lessthan, greaterthan, lessthanequal, greaterthanequal, dquoutes, stringx, whilex
 from lexer.lex_token import Token
 
 
@@ -57,6 +57,7 @@ class TzScriptLexer(Lexer):
         RETURN,
         CALLEDBY,
         EQUAL,
+        NOTEQUAL,
         EQUALEQUAL,
         LESSTHANEQUAL,
         GREATERTHANEQUAL,
@@ -91,6 +92,7 @@ class TzScriptLexer(Lexer):
     RBRACKET = r'\]'
     ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
     EQUALEQUAL = r'\=\='
+    NOTEQUAL = r'\!\='
     EQUAL = r'\='
     LESSTHANEQUAL = r'\<\='
     GREATERTHANEQUAL = r'\>\='
@@ -191,7 +193,7 @@ class TzScriptLexer(Lexer):
 
 
 map_to_terminals_names = {'CONTRACT': contract.Name, 'ID': idx.Name, 'COLON': colon.Name, 'SEMICOLON': semi.Name, 'COMMA': comma.Name, 'INTEGER': num.Name, 'LPAREN': opar.Name, 'RPAREN': cpar.Name, 'LBRACE': ocur.Name, 'RBRACE': ccur.Name, 'LBRACKET': opar.Name, 'RBRACKET': cpar.Name, 'PLUS': plus.Name, 'STAR': star.Name, 'ENTRY': entry.Name, 'FUNC': func.Name, 'LET': let.Name, 'IF': ifx.Name, 'ELSE': elsex.Name, 'TYPE': typex.Name, 'STRING': typex.Name,
-                          'NAT': typex.Name, 'INT': typex.Name, 'OPTIONAL': typex.Name, 'BOOL': typex.Name, 'EQUALEQUAL': equalequal.Name, 'LESSTHAN': lessthan.Name, 'GREATERTHAN': greaterthan.Name, 'LESSTHANEQUAL': lessthanequal.Name, 'GREATERTHANEQUAL': greaterthanequal.Name, 'EQUAL': equal.Name, 'MINUS': minus.Name, 'DIV': div.Name, 'RETURN': returnx.Name, 'ADDRESS': typex.Name, 'DOUBLEQUOTES': dquoutes.Name, 'STRINGTEXT': stringx.Name, 'WHILE': whilex.Name}
+                          'NAT': typex.Name, 'INT': typex.Name, 'OPTIONAL': typex.Name, 'BOOL': typex.Name, 'NOTEQUAL': iniquelaty.Name, 'EQUALEQUAL': equalequal.Name, 'LESSTHAN': lessthan.Name, 'GREATERTHAN': greaterthan.Name, 'LESSTHANEQUAL': lessthanequal.Name, 'GREATERTHANEQUAL': greaterthanequal.Name, 'EQUAL': equal.Name, 'MINUS': minus.Name, 'DIV': div.Name, 'RETURN': returnx.Name, 'ADDRESS': typex.Name, 'DOUBLEQUOTES': dquoutes.Name, 'STRINGTEXT': stringx.Name, 'WHILE': whilex.Name}
 
 
 def process_lexer_tokens(lexer_tokens) -> list[Token]:
